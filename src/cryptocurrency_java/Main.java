25ff2aa17869ea06fe0dcb89ce9be08fd8c095a2
Andrew Mingst <amingst97@gmail.com>
2018-12-06 12:36:50 -0500

    Updated Main.java
    Added main menu boilerplate to Main.java.

--- src/cryptocurrency_java/Main.java
+++ src/cryptocurrency_java/Main.java
@@ -1,9 +1,39 @@
 package cryptocurrency_java;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner menuInputScanner = new Scanner(System.in);
+		BlockChain blockChain = new BlockChain("Andrew");
 		
+		
+		while (true) {
+			System.out.println("Choose an option: ");
+			System.out.println("1: Add a new transaction");
+			System.out.println("2: Print blockchain contents");
+			System.out.println("q: Quit");
+			String menuInput = menuInputScanner.nextLine();
+			
+			switch (menuInput) {
+				case "1":
+					System.out.println("1");
+					break;
+				
+				case "2":
+					System.out.println("2");
+					break;
+				
+				case "q":
+					System.exit(0);
+					break;
+				
+				default: 
+					System.out.println("Invalid option");
+					break;
+			}
+		}
 	}
 
 }
